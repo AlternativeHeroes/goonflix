6521fc51e72982b7442c90c2a44d0374fbcc0494
Andrew Dai <theman@andrewdai.co>
2016-02-15 18:55:10 -0500

    add wireframe for edit user profile page

---
+++ app/src/main/java/com/goonsquad/goonflix/EditUserProfile.java
@@ -0,0 +1,13 @@
+package com.goonsquad.goonflix;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class EditUserProfile extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+    }
+}
